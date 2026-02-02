10e538dee3838dd23f9d9c0c3886446bfa5f24ed
Daniel Fernandes <202100507@estudantes.ips.pt>
2026-02-02 17:45:19 +0000

    Some fixes

--- Models/ResetPasswordViewModel.cs
+++ Models/ResetPasswordViewModel.cs
@@ -3,11 +3,13 @@ public class ResetPasswordViewModel
 {
     public string Token { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "A nova palavra-passe é obrigatória.")]
     [DataType(DataType.Password)]
+    [MinLength(6, ErrorMessage = "A palavra-passe deve ter pelo menos 6 caracteres.")]
     public string NewPassword { get; set; }
 
-    [Compare("NewPassword")]
+    [Required(ErrorMessage = "Confirme a sua palavra-passe.")]
     [DataType(DataType.Password)]
+    [Compare("NewPassword", ErrorMessage = "As palavras-passe não coincidem.")]
     public string ConfirmPassword { get; set; }
 }
