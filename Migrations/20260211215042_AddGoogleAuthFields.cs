9bb2ebad5af8f0055f488e826038dddb059681e9
Leite0708 <brunoleitepsies@gmail.com>
2026-02-11 21:58:12 +0000

    Google Auth Fix

---
+++ Migrations/20260211215042_AddGoogleAuthFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoCityWaste.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGoogleAuthFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthProvider",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProviderUserId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthProvider",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProviderUserId",
+                table: "Users");
+        }
+    }
+}
