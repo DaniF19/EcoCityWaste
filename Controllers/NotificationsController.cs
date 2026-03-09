be80773157731427c7c8ffbefc1cfdc250caa2e3
Daniel Fernandes <94930656+DaniF19@users.noreply.github.com>
2026-03-09 20:05:04 +0000

    servico de notificacoes criticas contentores

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,51 @@
+using EcoCityWaste.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoCityWaste.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public NotificationsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var notifications = await _context.Notifications
+                .OrderByDescending(n => n.CreatedAt)
+                .ToListAsync();
+
+            return View(notifications);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MarkAsRead(int id)
+        {
+            var notification = await _context.Notifications.FindAsync(id);
+
+            if (notification != null)
+            {
+                notification.IsRead = true;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ClearAll()
+        {
+            var notifications = await _context.Notifications.ToListAsync();
+
+            _context.Notifications.RemoveRange(notifications);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
