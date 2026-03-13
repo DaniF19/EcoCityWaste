ce921bc2f29c4e2b5efec5ae54cffbf501a56030
Bruno Leite <202100504@estudantes.ips.pt>
2026-03-13 17:31:38 +0000

    DB Migrations

---
+++ Migrations/20260313172518_AddReportOccurrence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoCityWaste.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReportOccurrence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Occurrences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContainerCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OccurrenceType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReportDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Occurrences", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Occurrences");
+        }
+    }
+}
