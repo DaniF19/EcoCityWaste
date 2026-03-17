a8eca462c5b0e4ce02a0d4c8a68d71f4d74d460e
Bruno Leite <202100504@estudantes.ips.pt>
2026-03-17 19:19:16 +0000

    little report fix

--- Controllers/OccurrencesController.cs
+++ Controllers/OccurrencesController.cs
@@ -25,15 +25,32 @@ namespace EcoCityWaste.Controllers
                     c.Code,
                     c.Location,
                     c.Type,
-                    Status = c.Status.ToString(),
-                    FillLevel = c.FillLevel
+                    c.Status,
+                    c.FillLevel
+                }).ToList();
+
+            var translate = containers
+                .Select(c => new {
+                    c.Code,
+                    c.Location,
+                    c.Type,
+                    Status = c.Status switch
+                    {
+                        Container.ContainerStatus.Good => "Bom",
+                        Container.ContainerStatus.Full => "Cheio",
+                        Container.ContainerStatus.Empty => "Vazio",
+                        Container.ContainerStatus.Broken => "Avariado",
+                        Container.ContainerStatus.Maintenance => "Manutenção",
+                        _ => "Desconhecido"
+                    },
+                    c.FillLevel
                 }).ToList();
 
             // Enviamos a lista para criar as <option> do HTML
-            ViewBag.ContainersList = containers;
+            ViewBag.ContainersList = translate;
 
             // Enviamos em formato JSON para o JavaScript conseguir ler no browser
-            ViewBag.ContainersJson = JsonSerializer.Serialize(containers);
+            ViewBag.ContainersJson = JsonSerializer.Serialize(translate);
 
             return View(new ReportOccurrenceViewModel());
         }
