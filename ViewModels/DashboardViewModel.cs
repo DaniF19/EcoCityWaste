06a23862ae67e28be99f8632cee520a8f5314b1f
PedroTavares202000290 <202000290@estudantes.ips.pt>
2026-03-23 21:08:53 +0000

    Dashboard de indicadores

---
+++ ViewModels/DashboardViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace EcoCityWaste.ViewModels
+{
+    public class DashboardViewModel
+    {
+        // Ocorrências
+        public int TotalOcorrencias { get; set; }
+        public int Pendente { get; set; }
+        public int EmAnalise { get; set; }
+        public int EmResolucao { get; set; }
+        public int Resolvido { get; set; }
+        public int Rejeitado { get; set; }
+
+        // Contentores
+        public int TotalContentores { get; set; }
+        public int ContentoresCriticos { get; set; }
+
+        // Indicadores temporais
+        public int OcorrenciasHoje { get; set; }
+        public int OcorrenciasSemana { get; set; }
+
+        // Indicadores Ambientais
+        public double NivelMedioEnchimento { get; set; }
+        public double PercentagemCriticos { get; set; }
+        public Dictionary<string, double> NivelMedioPorTipo { get; set; } = new();
+    }
+}
