578442f291c665a6cc26f2863bcf8998ad014bfd
Leite0708 <brunoleitepsies@gmail.com>
2026-03-31 15:09:36 +0100

    Report UI Update

--- ViewModels/ReportOccurrenceViewModel.cs
+++ ViewModels/ReportOccurrenceViewModel.cs
@@ -14,6 +14,6 @@ namespace EcoCityWaste.ViewModels
         [MinLength(10, ErrorMessage = "A descrição deve ter pelo menos 10 caracteres.")]
         public string Description { get; set; } = string.Empty;
 
-        public IFormFile Photo { get; set; }
+        public IFormFile? Photo { get; set; }
     }
 }
